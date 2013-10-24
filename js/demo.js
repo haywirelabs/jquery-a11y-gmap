830f4d9c48a83f955730ef88b602fa0ff864e6af
Joe Osborne <josborne@haywirelabs.com>
2013-10-24 13:46:22 -0400

    add map image

--- js/demo.js
+++ js/demo.js
@@ -4,13 +4,35 @@ $( document ).ready(function() {
   alert("start map.");
 */
 
-  //Set Options at Runtime.
+  /*
+  * Set Options at Runtime.
+  */
+
   var options = {
     width   : 300,
     height  : 225
   };
 
-  $( ".map" ).displayA11yGmap(options);
+  /*
+  * set Map Target.
+  * Can be either as either a
+  *
+  * comma-separated {latitude,longitude} pair (e.g. "40.714728,-73.998672")
+  *
+  * or a
+  *
+  * string address (e.g. "city hall, new york, ny")
+  *
+  * identifying a unique location
+  */
+
+  var target = "catonsville, maryland";
+
+  /*
+  * Identify an HTML element to create the map in.
+  */
+
+  $( ".map" ).displayA11yGmap(target, options);
 
 /*
   alert("end map.");
--- js/jquery-a11y-gmap.js
+++ js/jquery-a11y-gmap.js
@@ -1,18 +1,31 @@
 (function( $ ) {
-  $.fn.displayA11yGmap = function( options ) {
+  $.fn.displayA11yGmap = function( target , options ) {
 
+//    alert(target);
 //    alert(options);
 
     //defaults
     var settings = $.extend({
-      width   : 50,
-      height  : 50,
+      width         : 50,
+      height        : 50,
+      map_image_url : 'http://maps.googleapis.com/maps/api/staticmap?',
+      zoom          : 12,
+      sensor        : 'false'
     }, options );
 
+    map_image_url = settings.map_image_url + 'center=' + target;
+    map_image_url += '&zoom=' + settings.zoom;
+    map_image_url += '&size=' + settings.width + 'x' + settings.height;
+    map_image_url += '&sensor=' + settings.sensor;
+
+    map_image_element = '<img src="' + map_image_url + '" alt="map-image" />';
+
+    alert(map_image_url);
 
     return this.each(function() {
       $(this).css("width", settings.width);
       $(this).css("height", settings.height);
+      $(this).html(map_image_element);
     });
 
   };
