0bfdad231a27152fd495949b004376c5cc0ba1ed
Joe Osborne <josborne@haywirelabs.com>
2013-10-24 12:20:16 -0400

    add CCS to de-ugly

--- js/jquery-a11y-gmap.js
+++ js/jquery-a11y-gmap.js
@@ -1,7 +1,17 @@
 (function( $ ) {
-  $.fn.displayA11yGmap = function() {
+  $.fn.displayA11yGmap = function( options ) {
+
+    //defaults
+    var settings = $.extend({
+      width : "50px",
+      height : "50px",
+    }, options );
+
+
     return this.filter( "a" ).each(function() {
       $( this ).append( " (" + $( this ).attr( "href" ) + ")" );
     });
+
+
   };
 }( jQuery ));
