da3e4ac42e2a351691343427d36a4156fe028bc9
Joe Osborne <josborne@haywirelabs.com>
2013-11-13 12:51:45 -0500

    simplify html entity

--- js/jquery-a11y-gmap.js
+++ js/jquery-a11y-gmap.js
@@ -24,7 +24,7 @@
       //alert(target);
       
       if( target === undefined ){
-        target = $(this).attr("data-a11y-gmap-location");
+        target = $(this).attr("data-location");
       }    
       
       map_image_url = settings.map_image_url + 'center=' + target;
